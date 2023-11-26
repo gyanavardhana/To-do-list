7a7d236cda2520d63d36c82e89d933cac91af0ec
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2023-11-26 19:24:13 +0530

    s

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('./static'));
 
-// Connect to MongoDB
+// Connect to MongoDB (replace with your connection string)
 mongoose.connect('mongodb://127.0.0.1:27017/tasktracker', {})
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error(err));
